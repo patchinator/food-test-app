6fa6691606c1ae0390c16c443548fd30e7717e73
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-09-11 15:06:03 +0100

    added Navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,20 @@
+import style from "./Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <h2></h2>
+      </div>
+      <div>
+        <ul>
+          <li>Recipes</li>
+          <li>Create Recipe</li>
+          <li>Login</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
