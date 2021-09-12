4be7469c642576ae3bb9aabb607412376c3df1e0
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-09-12 02:38:55 +0100

    added conditional function so that localstorage functions clientside after conditional serverside check

--- store/auth-context.js
+++ store/auth-context.js
@@ -1,24 +1,34 @@
 import { useState } from "react";
 import React from "react";
+import { useRouter } from "next/router";
 
 const AuthContext = React.createContext({
-  token: '',
+  token: "",
   isLoggedIn: false,
   login: (token) => {},
   logout: () => {},
-})
+});
 
 export const AuthContextProvider = (props) => {
-  // undefined token (not logged in) or already stored token (refresh page)
-  const initialToken = localStorage.getItem('token');
+  const router = useRouter();
+  // Because NextJS is serverside + clientside, conditional to check if on server
+  // before accessing localstorage
+  const getLocalStorageToken = () => {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("token");
+    } else {
+      return null;
+    }
+  };
+
   // if token, user is logged in, if no token, user not logged in
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState(getLocalStorageToken);
 
   const userLoggedIn = !!token;
 
   const loginHandler = (token) => {
     // store token in local storage
-    localStorage.setItem('token', token);
+    localStorage.setItem("token", token);
     // sets token to the token being received
     setToken(token);
   };
@@ -26,7 +36,8 @@ export const AuthContextProvider = (props) => {
     // removes token on logout
     setToken(null);
     // removes token from local storage
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
+    router.push("/")
   };
 
   const contextValue = {
@@ -34,11 +45,13 @@ export const AuthContextProvider = (props) => {
     isLoggedIn: userLoggedIn,
     login: loginHandler,
     logout: logoutHandler,
-  } 
+  };
 
-  return <AuthContext.Provider value={contextValue}>
-    {props.children}
-  </AuthContext.Provider>
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContext;
