6536600762d53270770ed868d7afd9d33ec01a81
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-11-03 16:52:41 +0000

    install sass to make styling easier

--- components/Recipes/RecipeDetails.js
+++ components/Recipes/RecipeDetails.js
@@ -1,15 +1,16 @@
 import style from "./RecipeDetails.module.css";
-import Image from "next/dist/client/image";
 
 const RecipeDetails = (props) => {
   return (
-    <div>
-      <h1>{props.title}</h1>
-      <p className={style.description}>{props.description}</p>
-      <p className={style.description}>{props.ingredients}</p>
-      <p className={style.time}>{props.time}</p>
-      <p className={style.difficulty}>{props.difficulty}</p>
-      <Image src={props.image} alt={props.title}></Image>
+    <div className={style.details_wrapper}>
+      <div className={style.details}>
+        <h1>{props.title}</h1>
+        <p className={style.description}>{props.description}</p>
+        <p className={style.ingredients}>{props.ingredients}</p>
+        <p className={style.time}>{props.time}</p>
+        <p className={style.difficulty}>{props.difficulty}</p>
+        <img src={props.image} alt={props.title} />
+      </div>
     </div>
   );
 };
