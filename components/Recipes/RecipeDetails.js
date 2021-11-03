5d6bd4b76e2c27736d2252eeb08e21afb44db799
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-11-03 17:09:17 +0000

    added basic styling to details page

--- components/Recipes/RecipeDetails.js
+++ components/Recipes/RecipeDetails.js
@@ -1,15 +1,25 @@
-import style from "./RecipeDetails.module.css";
+import style from "./RecipeDetails.module.scss";
+import Link from "next/link";
 
 const RecipeDetails = (props) => {
   return (
     <div className={style.details_wrapper}>
       <div className={style.details}>
-        <h1>{props.title}</h1>
-        <p className={style.description}>{props.description}</p>
+        <div className={style.details_header}>
+          <h1>{props.title}</h1>
+          <p className={style.difficulty}>{props.difficulty}</p>
+        </div>
+        <div className={style.details_image}>
+          <img src={props.image} alt={props.title} />
+          <p className={style.time}>{props.time} mins</p>
+        </div>
+        <p>Ingredients:</p>
         <p className={style.ingredients}>{props.ingredients}</p>
-        <p className={style.time}>{props.time}</p>
-        <p className={style.difficulty}>{props.difficulty}</p>
-        <img src={props.image} alt={props.title} />
+        <p>Recipe:</p>
+        <p className={style.description}>{props.description}</p>
+        <div className={style.details_footer}>
+          <Link href="/">Back</Link>
+        </div>
       </div>
     </div>
   );
