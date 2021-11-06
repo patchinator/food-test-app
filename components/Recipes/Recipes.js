c2efe79a57259dbfe22b9ffc49bda1349453008e
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-11-06 17:44:28 +0000

    fixed margin on recipes component

--- components/Recipes/Recipes.js
+++ components/Recipes/Recipes.js
@@ -9,7 +9,6 @@ const Recipes = (props) => {
   const [librarySize, setLibrarySize] = useState(12);
   const [searchRecipe, setSearchRecipe] = useState("");
   const formRef = useRef();
-  let recipeArr = [];
 
   const showMoreRecipesHandler = () => {
     const newSize = librarySize + 8;
